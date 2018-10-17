9fa6a097055f241e503c1c5ace33c2f8c1254a38
DESKTOP-T82SLBV\Jun <ldh243@gmail.com>
2018-10-18 00:58:58 +0700

    Upgrade chart (Select date)

--- app/src/main/java/com/example/jun/antiphone/ProfileActivity.java
+++ app/src/main/java/com/example/jun/antiphone/ProfileActivity.java
@@ -1,11 +1,17 @@
 package com.example.jun.antiphone;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
@@ -18,6 +24,7 @@ import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
@@ -27,40 +34,61 @@ import util.DateTimeUtils;
 
 public class ProfileActivity extends AppCompatActivity {
     LineGraphSeries<DataPoint> series;
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        double y, x;
-        x = 0;
+        drawChart();
+        setTotalTimeHolding();
 
+
+
+    }
+    private void drawChart() {
         GraphView graph = findViewById(R.id.graph1);
-//        graph.setTitleColor(Color.BLACK);
-//        graph.setBackgroundColor(Color.CYAN);
-//        graph.setTitle("DAY LA CHART");
-
         graph.getGridLabelRenderer().setGridColor(Color.GRAY);
         graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLACK);
         graph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
         graph.getGridLabelRenderer().setVerticalLabelsAlign(Paint.Align.LEFT);
-        graph.getGridLabelRenderer().setVerticalAxisTitle("Time");
-        graph.getGridLabelRenderer().setVerticalAxisTitleColor(Color.RED);
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Time (m)");
+        graph.getGridLabelRenderer().setVerticalAxisTitleColor(Color.BLACK);
+        graph.getGridLabelRenderer().setPadding(40);
         ProfileUserDAO dao = new ProfileUserDAO();
+
+        Date date1 = null;
+        TextView txtDate = findViewById(R.id.txtDate);
+
+        try {
+        String sDate1 = txtDate.getText().toString();
+            date1 = new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        Date[] listDay = dao.get7DaysBefore(date1);
+
+        String title = listDay[0].toString().split(" ")[2] + ". "
+                + listDay[0].toString().split(" ")[1] + " - "
+                + listDay[6].toString().split(" ")[2] + ". "
+                + listDay[6].toString().split(" ")[1];
+
+        graph.setTitle(title);
+        graph.setTitleColor(Color.BLACK);
         DataPoint [] list = dao.chartHoldingInWeek("123");
-        String [] dayOfWeek = dao.getDayOfWeek();
-        Date[] listDate = dao.get7DaysBefore();
+        String [] dayOfWeek = dao.getDayOfWeek(date1);
         series = new LineGraphSeries<>(list);
         graph.addSeries(series);
         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-
-        staticLabelsFormatter.setHorizontalLabels(new String[]{dayOfWeek[0], dayOfWeek[1], dayOfWeek[2], dayOfWeek[3], dayOfWeek[4], dayOfWeek[5], dayOfWeek[6]});
+        staticLabelsFormatter.setHorizontalLabels(dayOfWeek);
         graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-        graph.getViewport().setMaxX(listDate[6].getTime()+10000000);
-        graph.getViewport().setMinX(listDate[0].getTime()-10000000);
         graph.getViewport().setXAxisBoundsManual(true);
-        setTotalTimeHolding();
+        graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.HORIZONTAL);
+        graph.getGridLabelRenderer().setHorizontalAxisTitleTextSize(11);
     }
+
     private void setTotalTimeHolding() {
         Intent intent = this.getIntent();
         Bundle userInfo = intent.getBundleExtra("USERINFO");
@@ -71,8 +99,29 @@ public class ProfileActivity extends AppCompatActivity {
         String totalTime = dao.getTotalTimeHolding(userId);
         TextView txtTotalTime = findViewById(R.id.txtTotalTime);
         txtTotalTime.setText(totalTime);
+    }
 
+    public void changeDate(View view) {
+        mDisplayDate = findViewById(R.id.txtDate);
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dialog = new DatePickerDialog(
+                ProfileActivity.this,
+                AlertDialog.THEME_DEVICE_DEFAULT_LIGHT,
+                mDateSetListener,
+                year,month,day);
+        dialog.show();
 
-//        Log.d("Tagggggg", dateStartWeek + " " + dateEndWeek);
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = day + "/" +  month + "/" + year;
+                mDisplayDate.setText(date);
+                drawChart();
+            }
+        };
     }
 }
--- app/src/main/java/model/ProfileUserDAO.java
+++ app/src/main/java/model/ProfileUserDAO.java
@@ -13,10 +13,11 @@ import java.util.List;
 import util.DateTimeUtils;
 
 public class ProfileUserDAO {
-    public Date[] get7DaysBefore() {
+    public Date[] get7DaysBefore(Date date) {
         Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
         calendar.add(Calendar.DATE, -6);
-        Date dateStartWeek = calendar.getTime();
+        Date d1 = calendar.getTime();
         calendar.add(Calendar.DATE, 1);
         Date d2 = calendar.getTime();
         calendar.add(Calendar.DATE, 1);
@@ -28,28 +29,27 @@ public class ProfileUserDAO {
         calendar.add(Calendar.DATE, 1);
         Date d6 = calendar.getTime();
         calendar.add(Calendar.DATE, 1);
-        Date dateEndWeek = calendar.getTime();
-        return new Date[]{dateStartWeek, d2, d3,  d4, d5, d6, dateEndWeek};
+        Date d7 = calendar.getTime();
+        return new Date[]{d1, d2, d3,  d4, d5, d6, d7};
     }
 
     public DataPoint[] chartHoldingInWeek(String userId){
-        Date[] listDate = get7DaysBefore();
         DataPoint [] list = new DataPoint[]{
-                new DataPoint(listDate[0], 3),
-                new DataPoint(listDate[1], 1),
-                new DataPoint(listDate[2], 7),
-                new DataPoint(listDate[3], 11),
-                new DataPoint(listDate[4], 18),
-                new DataPoint(listDate[5], 4),
-                new DataPoint(listDate[6], 3)
+                new DataPoint(1, 3),
+                new DataPoint(2, 1),
+                new DataPoint(3, 7),
+                new DataPoint(4, 11),
+                new DataPoint(5, 18),
+                new DataPoint(6, 4),
+                new DataPoint(7, 0)
         };
         return list;
     }
 
-    public String [] getDayOfWeek() {
-        Date[] listDate = get7DaysBefore();
+    public String [] getDayOfWeek(Date date) {
+        Date[] listDate = get7DaysBefore(date);
         String [] dayOfWeek = new String[7];
-        for (int i = 0; i < listDate.length; i++) {
+        for (int i = 0; i < 7; i++) {
             dayOfWeek[i] = listDate[i].toString().split( " ")[0];
         }
         return dayOfWeek;
